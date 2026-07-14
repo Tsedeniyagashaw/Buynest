ee8791af34021e5601ad0ed6a380e5cd03670fb4
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2026-07-15 02:38:49 +0300

    improve profile management

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -154,4 +154,63 @@ const updateProfile = async (req, res) => {
     }
 };
 
-module.exports = { register, login, authorizeRoles, getProfile, updateProfile };
+const changePassword = async (req, res) => {
+    try {
+
+        const { currentPassword, newPassword } = req.body;
+
+
+        const user = await User.findById(req.user.id);
+
+
+        if(!user){
+
+            return res.status(404).json({
+                message:"User not found"
+            });
+
+        }
+
+
+
+        const isMatch = await bcrypt.compare(
+            currentPassword,
+            user.password
+        );
+
+
+        if(!isMatch){
+
+            return res.status(400).json({
+                message:"Current password is incorrect"
+            });
+
+        }
+
+        const hashedPassword = await bcrypt.hash(
+            newPassword,
+            10
+        );
+
+
+        user.password = hashedPassword;
+
+
+        await user.save();
+
+
+        res.json({
+            message:"Password changed successfully"
+        });
+
+
+    } catch(error){
+
+        res.status(500).json({
+            message:error.message
+        });
+
+    }
+};
+
+module.exports = { register, login, authorizeRoles, getProfile, updateProfile, changePassword };
